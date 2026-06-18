c210157351c4526acc143c215a0537582be3924c
rs-rocha <robsonrocha.unb@gmail.com>
2026-06-17 21:45:30 -0300

    Criado a logica da funcao cadastrarFuncionario no arquivocracha.js

---
+++ Atividade6/cracha.js
@@ -0,0 +1,27 @@
+function cadastrarFuncionario(){
+    let nome = document.getElementById("nome").value;    
+    let idade = document.getElementById("idade").value;    
+    let cargo = document.getElementById("cargo").value;    
+    let departamento = document.getElementById("departamento").value;
+    let acessoRestrito = document.getElementById("acessoRestrito").value;    
+    
+    console.log(
+       "Nome: " + nome + "\n" + 
+       "Idade: " + idade + "\n" +
+       "Cargo: " + cargo + "\n" +
+       "Departamento: " + departamento + "\n" +
+       "Acesso Restrito: " + acessoRestrito + "\n" +
+       "\n"
+    )
+
+    alert(
+        "Nome: " + nome + "\n" + 
+        "Idade: " + idade + "\n" +
+        "Cargo: " + cargo + "\n" +
+        "Departamento: " + departamento + "\n" +
+        "Acesso Restrito: " + acessoRestrito + "\n" +
+        "\n" 
+     );
+
+    
+}
