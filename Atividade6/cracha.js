68c85d1f295972cee2f331bce25623125faa4c2a
rs-rocha <robsonrocha.unb@gmail.com>
2026-06-18 19:14:05 -0300

    Identar codigo do cracha.js

--- Atividade6/cracha.js
+++ Atividade6/cracha.js
@@ -1,27 +1,25 @@
 function cadastrarFuncionario(){
-    let nome = document.getElementById("nome").value;    
-    let idade = document.getElementById("idade").value;    
-    let cargo = document.getElementById("cargo").value;    
-    let departamento = document.getElementById("departamento").value;
-    let acessoRestrito = document.getElementById("acessoRestrito").value;    
+   let nome = document.getElementById("nome").value;    
+   let idade = document.getElementById("idade").value;    
+   let cargo = document.getElementById("cargo").value;    
+   let departamento = document.getElementById("departamento").value;
+   let acessoRestrito = document.getElementById("acessoRestrito").value;    
     
-    console.log(
+   console.log(
        "Nome: " + nome + "\n" + 
        "Idade: " + idade + "\n" +
        "Cargo: " + cargo + "\n" +
        "Departamento: " + departamento + "\n" +
        "Acesso Restrito: " + acessoRestrito + "\n" +
        "\n"
-    )
+   )
 
-    alert(
+   alert(
         "Nome: " + nome + "\n" + 
         "Idade: " + idade + "\n" +
         "Cargo: " + cargo + "\n" +
         "Departamento: " + departamento + "\n" +
         "Acesso Restrito: " + acessoRestrito + "\n" +
         "\n" 
-     );
-
-    
+   );   
 }
