4f180e1db3420c14103cabdc930c47e65bd387db
rs-rocha <robsonrocha.unb@gmail.com>
2026-06-17 21:07:35 -0300

    Separando o codigo script para formulario.js

---
+++ Atividade5/formulario.js
@@ -0,0 +1,8 @@
+function imprimirNome(){
+    window.alert("Usuario Cadastrado!");
+    //window.prompt("Digite sua experiencia com nosso site: ")
+    //console.log("Seu formulario foi enviado")
+    let nome = document.getElementById("nome").value;
+    //console.log("Seu nome é: ", nome);
+    alert("Aluno cadastrado!\n\nNome do aluno: " + nome);
+}
