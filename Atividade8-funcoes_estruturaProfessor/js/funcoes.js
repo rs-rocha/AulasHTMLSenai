da93dfd4fa098b5057482614645f0e81a1973371
rs-rocha <robsonrocha.unb@gmail.com>
2026-06-25 21:50:06 -0300

    Implementa o calculo de imposto de importação

--- Atividade8-funcoes_estruturaProfessor/js/funcoes.js
+++ Atividade8-funcoes_estruturaProfessor/js/funcoes.js
@@ -3,6 +3,7 @@ crtl+shif+p - mostra comandos do vscode
 crtl+p - mostra pesquisa
 */
 
+
 function soma() {
     let primeiroNumero = document.getElementById("primeiro-numero").value;
     let segundoNumero = document.getElementById("segundo-numero").value;
@@ -73,4 +74,50 @@ function apagarLocalStorage(){
     if(confirmaApagar){
         localStorage.clear();
     }
+}
+
+/*
+    Nova função de calculo de imposto de importação
+
+    Se o produto custar menos que $50 (cinquenta dolares)
+    Você vai ter que pagar o valor produto, mais 20%
+
+    Se o produto custar mais do que $50
+    Você vai ter que pagar o valor do produto +20%, depois mais 90% sobre o resultado
+
+    Seja salvo num novo objeto, dentro do local storage, seu hitórico de coversão.
+
+    Ex: histórico de calculos matemáticos, e histórico de conversões
+
+ */
+
+function calcularImposto(){
+    let valorProduto = document.getElementById("valor-produto").value;
+    let menorCinquenta = Number(valorProduto) <= 50;
+    let valor = Number(valorProduto);
+    let valorFinal;
+    let calculo;
+
+    if(menorCinquenta){
+        valorFinal = valor*1.2;
+        calculo = "valor + valor x 20%";
+    }else{
+        valorFinal = 1.9*(valor*1.2);
+        calculo = "1.9 x (valor x 1.2)";
+    }
+    console.log(valorFinal);
+    document.getElementById("valor-total").innerText = valorFinal;
+    historicoCompras(valor, valorFinal);
+}
+
+function historicoCompras(valor, valorFinal){
+    operacao = {
+        valorInicial: valor,
+        valorFinal: valorFinal
+    };
+
+    let historicoLocal = JSON.parse(localStorage.getItem("historicoCompras")) || [];
+    historicoLocal.push(operacao);
+    localStorage.setItem("historicoCompras", JSON.stringify(historicoLocal));
+
 }
